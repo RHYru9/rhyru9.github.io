1a776e42cc507d1ac2f210cdb30bfbc1180aec96
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-08-21 06:45:34 +0700

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,1 +1,5 @@
-Hello, I'm a ghost.
+console.log('Test');
+var testDiv = document.createElement('div');
+testDiv.textContent = 'JavaScript payload executed!';
+testDiv.style.color = 'green';
+document.body.appendChild(testDiv);
