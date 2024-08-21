93c33d60485f38217a3e9d64c5c74464e437bbcd
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-08-21 07:06:12 +0700

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,2 +1,9 @@
-console.log('1');    
-console.error('1'); 
+document.addEventListener('DOMContentLoaded', () => {
+  const encodedScript = getParameterByName('script');
+  if (encodedScript) {
+    const decodedScript = decodeURIComponent(encodedScript);
+    const script = document.createElement('script');
+    script.textContent = decodedScript;
+    document.head.appendChild(script);
+  }
+});
