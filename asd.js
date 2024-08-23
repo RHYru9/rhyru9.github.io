058f939d3adfa0482939363b4c70b8f4839deb0c
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-08-23 15:36:36 +0700

    Update asd.js

--- asd.js
+++ asd.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const encodedScript = getParameterByName('script');
-  if (encodedScript) {
-    const decodedScript = decodeURIComponent(encodedScript);
-    const script = document.createElement('script');
-    script.textContent = decodedScript;
-    document.head.appendChild(script);
-  }
-});
+hello, i'm a ghost
