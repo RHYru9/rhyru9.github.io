77b6f200169d5a88599f0802158795da65087a71
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-10-25 01:44:28 +0700

    Update test.js

--- js/test.js
+++ js/test.js
@@ -1,24 +1,3 @@
-document.head.innerHTML = `<style>* {
-  margin: 0;
-  padding: 0;
-}
-.hacked {
-  background: black;
-  color: lime;
-  height: 100vh;
-  
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  text-align: center;
-  font-size: 3rem;
-}</style>`;
-
-document.body.innerHTML = `
-<div class="hacked">
-  <h1>test</h1>
-</div>
-`;
-
-window.addEventListener('click', e => alert('test'));
+<script type="text/javascript">
+alert("rhyru9");
+</script>
